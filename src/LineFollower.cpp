4dccf737dda986a22e3eca0ed9817cc8cb93a599
Raresh Nistor <nistorrare@ecs.vuw.ac.nz>
2019-05-31 09:22:48 +1200

    Q2 complete. Grade bumped to 67%

--- src/LineFollower.cpp
+++ src/LineFollower.cpp
@@ -12,9 +12,10 @@
 #include "Camera.h"
 #include "LineFollower.h"
 
-void LineFollower::followTheLine() {
+int getLineCenter(int y) {
+    // Threshold the line
     int whiteCount = 0;
-    Pixel* line = Camera::getLine(Camera::FB_HEIGHT * 2 / 3);
+    Pixel* line = Camera::getLine(y);
 
     bool error[Camera::FB_WIDTH];
 
@@ -28,20 +29,16 @@ void LineFollower::followTheLine() {
 
     delete[] line;
 
-    for (int x = 0; x < Camera::FB_WIDTH; x++) {
-        printf(error[x] ? "#" : " ");
+    if (whiteCount >= Camera::FB_WIDTH) {
+        return -1;
+    }
+
+    for (int x = 0; x < Camera::FB_WIDTH; x += 2) {
+        printf(error[x] ? "." : " ");
     }
     printf("\n");
 
-    // If robot has lost line completely, reverse
-    if (whiteCount >= Camera::FB_WIDTH) {
-        Drive::setSpeed(-0.25);
-        Drive::reverseAngle();
-        return;
-    }
-    Drive::setSpeed(0.5);
-
-    // Get the line
+    // Get the line position
     int lineStart = -1;
     int lineLength = 0;
     for (int i = 0; i < Camera::FB_WIDTH; i++) {
@@ -55,12 +52,34 @@ void LineFollower::followTheLine() {
         }
     }
 
-    int lineCenter = lineStart + lineLength / 2;
-    float angle = (float)(lineCenter - 160) / 160;
+    return lineStart + lineLength / 2;
+}
 
+void LineFollower::followTheLine() {
+    // Get the line
+    printf("top ");
+    int topLine = getLineCenter(Camera::FB_HEIGHT * 1 / 3);
+    if (topLine < 0) printf("\n");
+    printf("mid ");
+    int midLine = getLineCenter(Camera::FB_HEIGHT / 2);
+    if (midLine < 0) printf("\n");
+    printf("bot ");
+    int botLine = getLineCenter(Camera::FB_HEIGHT * 2 / 3);
+    if (botLine < 0) printf("\n");
+
+    int avgLine = (topLine + midLine + botLine) / 3;
+
+    // If robot has lost line completely, reverse
+    if (midLine < 0 || botLine < 0) {
+        Drive::setSpeed(-0.2);
+        sleep1(250);
+        Drive::reverseAngle();
+        return;
+    }
+
+    // Lel
+    float angle = (float)(avgLine - 160) / 160;
+
+    Drive::setSpeed(0.5);
     Drive::setAngle(-angle);
-
-    // Differential experimentation
-
-
 }
